74510701113719fe4ba2c68d8a089a79ab2148bc
PoojaDhingra <pooja.dhingra@spanservices.com>
2015-11-05 18:09:23 +0530

    creates test file.cs

---
+++ APITestProject/testFile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class testFile
+    {
+        internal const string KeywordApi = "_api";
+    }
+}
