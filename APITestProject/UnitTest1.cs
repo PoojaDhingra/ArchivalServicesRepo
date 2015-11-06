b625ade0444317ecd2a2487aec55a3cbfe3538fd
pooja.dhingra <pooja.dhingra@spanservices.com>
2015-11-06 10:41:49 +0530

    updatd project

--- APITestProject/UnitTest1.cs
+++ APITestProject/UnitTest1.cs
@@ -10,15 +10,8 @@ namespace APITestProject
         public void TestMethod1()
         {
             WebService2 ws = new WebService2();
-            string testResult = ws.HelloWorld1();
-            Assert.AreEqual("result confirmed", testResult);
+            ws.HelloWorld1();           
         }
-
-        [TestMethod]
-        public void BreakRoleInheritance()
-        {
-            WebService2 ws = new WebService2();
-            ws.BreakRoleInheritance("Dokumenter", "http://spivpc:9004/sites/DCM/_api/", "asharma", "span@123", "SPIVPC"); ;
-        }
+     
     }
 }
